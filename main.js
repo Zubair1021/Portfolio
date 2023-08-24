1079c8093d2f6bdea4676ea726f2488ce1e6186f
Muhammad Zubair <121050120+Zubair1021@users.noreply.github.com>
2023-08-24 10:15:17 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+// scrolling effect on main section skills
+
+var typed = new Typed(".typing",{
+    strings:["Frontened Developer","Programmer","Video Editor" ,"Teacher","GUI App Developer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+});
+
+//Logo scrolling effect on navbar
+var typed = new Typed(".por",{
+    strings:["Portfolio","ZJ Developer"],
+    typeSpeed:150,
+    backSpeed:60,
+    loop:true
+});
